b734263c63d01d46f8ab990460b72ba8dc4dccac
Nikita Shabankin <n.shabankin@gmail.com>
2021-10-13 22:19:07 +0300

    initial commit

--- tmstats/controls.py
+++ tmstats/controls.py
@@ -1,18 +1,22 @@
-from transfermarkt.spiders import Leaguespider, Teamspider, Playerspider, \
-    Tablespider
 import pathlib
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from transfermarkt.settings import team_fields, table_fields, league_fields, \
     player_fields
+from transfermarkt.spiders import Leaguespider, Teamspider, Playerspider, \
+    Tablespider
 
 
 class GetData:
 
     def __init__(self, league=None, year=None):
+        """
+        Receives a league name and a year to process throughout.
+        """
         self.league = league
         self.year = year
-        # make a dict with league names and their URL aliases
+
+        # make a dict with league names and their URL shortcuts
         self.leagues = {'epl': 'GB1',
                         'serie_a': 'IT1',
                         'la_liga': 'ES1',
@@ -22,6 +26,7 @@ class GetData:
                         'rpl': 'RU1',
                         'eredivisie': 'NL1'}
 
+        # make a dict with league names and their URL aliases
         self.tables = {'epl': 'premier-league',
                        'serie_a': 'serie-a',
                        'la_liga': 'laliga',
@@ -33,54 +38,57 @@ class GetData:
 
     def teams(self):
         """
-        Runs the spider to export the list of teams and their URLs to .csv
-        :return:
-
+        Runs the spider to export a list of teams and their URLs to .csv,
+        thus forming a pool of URLs for the next spider to crawl over.
         """
-        settings_teams = get_project_settings()
-        # set export fields for teams table
+        settings_teams = get_project_settings()  # from settings.py
+        # set export fields for teams
         settings_teams['FEED_EXPORT_FIELDS'] = league_fields
         # set filepath and filename for the .csv output
         settings_teams['FEEDS'] = {pathlib.Path(
             f'{str(self.league)}/'  # folder name
             f'{str(self.league)}_teams_{str(self.year)}.csv'):  # file name
                                        {'format': 'csv',  # format
-                                        'overwrite': True}}
+                                        'overwrite': True}}  # allow overwrite
         # process_teams
         process = CrawlerProcess(settings_teams)
-        process.crawl(Leaguespider,
+        process.crawl(Leaguespider,  # refer to the spider for teams crawling
                       input='inputargument',
-                      # put a short league alias as the only argument
+                      # received from leagues dict with league as the key
                       league_site=self.leagues[self.league])
         process.start()
 
     def table(self):
         """
-        Runs the spider to export the list of teams and their URLs to .csv
-        :return:
-
+        Runs the spider to export an up-to-date league table in a .csv file.
         """
-        settings_table = get_project_settings()
-        # set export fields for teams table
+        settings_table = get_project_settings()  # from settings.py
+        # set export fields for league table
         settings_table['FEED_EXPORT_FIELDS'] = table_fields
         # set filepath and filename for the .csv output
         settings_table['FEEDS'] = {pathlib.Path(
             f'{str(self.league)}/'  # folder name
             f'{str(self.league)}_table_{str(self.year)}.csv'):  # file name
                                        {'format': 'csv',  # format
-                                        'overwrite': True}}
+                                        'overwrite': True}}  # allow overwrite
         # process_teams
         process = CrawlerProcess(settings_table)
-        process.crawl(Tablespider,
+        process.crawl(Tablespider,  # refer to the spider for table crawling
                       input='inputargument',
-                      # put a short league alias as the only argument
+                      # received from the tables dict with league as the key
                       table=self.tables[self.league],
+                      # received from leagues dict with league as the key
                       league_site=self.leagues[self.league],
+                      # received with class initiation
                       year=self.year)
         process.start()
 
     def players(self):
-        settings_players = get_project_settings()
+        """
+        Form a .csv table with every player URLs for the next spider
+        to crawl over.
+        """
+        settings_players = get_project_settings()  # from settings.py
         # set export fields for players table
         settings_players['FEED_EXPORT_FIELDS'] = team_fields
         # set filepath and filename for the .csv output
@@ -91,7 +99,7 @@ class GetData:
                                           'overwrite': True}}
         # process_players
         process = CrawlerProcess(settings_players)
-        process.crawl(Teamspider,
+        process.crawl(Teamspider,  # refer to the spider for players crawling
                       input='inputargument',
                       # league name and year as arguments
                       league=self.league,
@@ -99,8 +107,11 @@ class GetData:
         process.start()
 
     def stats(self):
-        settings_stats = get_project_settings()
-        # set export fields for stats table
+        """
+        Crawls over the list of players and returns their stats in a .csv file.
+        """
+        settings_stats = get_project_settings()  # from settings.py
+        # set export fields for player stats
         settings_stats['FEED_EXPORT_FIELDS'] = player_fields
         # set filepath and filename for the .csv output
         settings_stats['FEEDS'] = {pathlib.Path(
@@ -110,30 +121,9 @@ class GetData:
                                         'overwrite': True}}
         # process_stats
         process = CrawlerProcess(settings_stats)
-        process.crawl(Playerspider,
+        process.crawl(Playerspider,  # refer to the spider for player stats
                       input='inputargument',
                       # league name and year as arguments
                       league=self.league,
                       year=self.year)
         process.start()
-
-    # the wrapper to make it run more times
-    '''def run_spider(spider):
-        def f(q):
-            try:
-                runner = crawler.CrawlerRunner()
-                deferred = runner.crawl(spider)
-                deferred.addBoth(lambda _: reactor.stop())
-                reactor.run()
-                q.put(None)
-            except Exception as e:
-                q.put(e)
-
-        q = Queue()
-        p = Process(target=f, args=(q,))
-        p.start()
-        result = q.get()
-        p.join()
-
-        if result is not None:
-            raise result'''
