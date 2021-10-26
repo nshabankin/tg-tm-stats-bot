55cba9d359ec86977c0c1fbea5fd69bfe8df8485
Nikita Shabankin <n.shabankin@gmail.com>
2021-10-27 02:13:57 +0300

    initial commit

--- scheduler.py
+++ scheduler.py
@@ -4,27 +4,23 @@ from tmstats import GetData
 
 q = Queue(connection=conn)
 # run update in the background
-update_epl = q.enqueue(GetData.stats(self=GetData(
-    'epl', '2021')),
-    'epl', '2021')
-update_bundesliga = q.enqueue(GetData.stats(self=GetData(
-    'bundesliga', '2021')),
-    'bundesliga', '2021')
-update_serie_a = q.enqueue(GetData.stats(self=GetData(
-    'serie_a', '2021')),
-    'serie_a', '2021')
-update_la_liga = q.enqueue(GetData.stats(self=GetData(
-    'la_liga', '2021')),
-    'la_liga', '2021')
-update_ligue_1 = q.enqueue(GetData.stats(self=GetData(
-    'ligue_1', '2021')),
-    'ligue_1', '2021')
-update_rpl = q.enqueue(GetData.stats(self=GetData(
-    'rpl', '2021')),
-    'rpl', '2021')
+epl = GetData.stats(GetData('epl', '2021'))
+bundesliga = GetData.stats(GetData('bundesliga', '2021'))
+serie_a = GetData.stats(GetData('serie_a', '2021'))
+la_liga = GetData.stats(GetData('la_liga', '2021'))
+ligue_1 = GetData.stats(GetData('ligue_1', '2021'))
+rpl = GetData.stats(GetData('rpl', '2021'))
+
+update_epl = q.enqueue(epl, 'epl', '2021')
+update_bundesliga = q.enqueue(bundesliga, 'bundesliga', '2021')
+update_serie_a = q.enqueue(serie_a, 'serie_a', '2021')
+update_la_liga = q.enqueue(la_liga, 'la_liga', '2021')
+update_ligue_1 = q.enqueue(ligue_1, 'ligue_1', '2021')
+update_rpl = q.enqueue(rpl, 'rpl', '2021')
 
 update = [update_epl, update_bundesliga, update_serie_a, update_la_liga,
           update_ligue_1, update_rpl]
+
 with Connection(conn):
     for league in update:
         w = Worker(league)
