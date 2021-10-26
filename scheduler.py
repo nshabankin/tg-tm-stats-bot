4e64acc9db12b0631467f51a3dfe52692023523a
Nikita Shabankin <n.shabankin@gmail.com>
2021-10-27 02:51:18 +0300

    initial commit

--- scheduler.py
+++ scheduler.py
@@ -12,16 +12,28 @@ ligue_1 = GetData.stats(GetData('ligue_1', '2021'))
 rpl = GetData.stats(GetData('rpl', '2021'))
 
 update_epl = q.enqueue(epl, 'epl', '2021')
+with Connection(conn):
+    w = Worker(update_epl)
+    w.work()
 update_bundesliga = q.enqueue(bundesliga, 'bundesliga', '2021')
+with Connection(conn):
+    w = Worker(update_bundesliga)
+    w.work()
 update_serie_a = q.enqueue(serie_a, 'serie_a', '2021')
+with Connection(conn):
+    w = Worker(update_serie_a)
+    w.work()
 update_la_liga = q.enqueue(la_liga, 'la_liga', '2021')
+with Connection(conn):
+    w = Worker(update_la_liga)
+    w.work()
 update_ligue_1 = q.enqueue(ligue_1, 'ligue_1', '2021')
+with Connection(conn):
+    w = Worker(update_ligue_1)
+    w.work()
 update_rpl = q.enqueue(rpl, 'rpl', '2021')
-
+with Connection(conn):
+    w = Worker(update_rpl)
+    w.work()
 update = [update_epl, update_bundesliga, update_serie_a, update_la_liga,
           update_ligue_1, update_rpl]
-
-with Connection(conn):
-    for league in update:
-        w = Worker(league)
-        w.work()
